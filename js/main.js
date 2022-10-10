31b755bd12fa9d5038db2a5e20bb961fabf8d15e
Cesar Moros <rdcesar442@gmail.com>
2022-10-10 05:16:18 -0300

    Finished mobile look

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-//Haz tú validación en javascript acá
+// const mainNavLinks = document.querySelectorAll('.main-nav__link');
+const mainNavCheckbox = document.querySelector('.main-nav-toggle');
+console.log('🚀 ~ mainNavCheckbox', mainNavCheckbox);
+// const hamburgerButton = document.querySelector('.main-header__hamburger-button-container')
+
+// mainNavLinks.forEach(link => {
+//   link.addEventListener('click', e => {
+//     if (getComputedStyle(hamburgerButton).display == "none" ) return;
+
+//   })
+// });
+
+document.addEventListener('click', e => {
+  if (
+      e.target.classList.contains('main-nav__link') ||
+      e.target.classList.contains('main-header__contact-link') ||
+      e.target.classList.contains('main-header__title-link')
+    ) {
+    mainNavCheckbox.checked = false;
+    }
+})
